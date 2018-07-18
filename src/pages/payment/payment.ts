24b8b7c5b5c23c54e4e0549ba07b60d53511a1c1
Unknown <jiaying1@me.com>
2018-07-18 10:37:40 +0200

    charts payment bar graphs

--- src/pages/payment/payment.ts
+++ src/pages/payment/payment.ts
@@ -1,7 +1,9 @@
 import { Component, AfterViewInit, OnDestroy, ViewChild, ElementRef, ChangeDetectorRef } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { NgForm } from '@angular/forms';
+// import { MapPage } from '../map/map';
 import { Http } from '@angular/http';
+import { Chart } from 'chart.js';
 
 /**
  * Generated class for the PaymentPage page.
@@ -12,13 +14,21 @@ import { Http } from '@angular/http';
 
 @IonicPage()
 
-@Component({ 
+@Component({
   selector: 'page-payment',
   templateUrl: 'payment.html',
- })
+})
 
 export class PaymentPage implements AfterViewInit, OnDestroy {
   @ViewChild('cardInfo') cardInfo: ElementRef;
+
+  @ViewChild('barCanvas') barCanvas;
+  @ViewChild('doughnutCanvas') doughnutCanvas;
+  @ViewChild('lineCanvas') lineCanvas;
+
+  barChart: any;
+  doughnutChart: any;
+  lineChart: any;
 
   card: any;
   cardHandler = this.onChange.bind(this);
@@ -33,61 +43,162 @@ export class PaymentPage implements AfterViewInit, OnDestroy {
   public receipt_email: string;
 
 
-  constructor(private cd: ChangeDetectorRef, public navCtrl: NavController, public navParams: NavParams, public http: Http ) {}
+  constructor(private cd: ChangeDetectorRef, public navCtrl: NavController, public navParams: NavParams, public http: Http) { }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad PaymentPage');
-  }
-  
-  ngAfterViewInit() {
-    const style = {
-      base: {
-        lineHeight: '24px',
-        fontFamily: 'monospace',
-        fontSmoothing: 'antialiased',
-        fontSize: '19px',
-        '::placeholder': {
-          color: 'purple'
+
+    this.barChart = new Chart(this.barCanvas.nativeElement, {
+      type: 'bar',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
         }
       }
-    };
 
-    this.card = elements.create('card', { style });
-    this.card.mount(this.cardInfo.nativeElement);
+    });
 
-    this.card.addEventListener('change', this.cardHandler);
-  }
+    this.doughnutChart = new Chart (this.doughnutCanvas.nativeElement, {
 
-  ngOnDestroy() {
-    this.card.removeEventListener('change', this.cardHandler);
-    this.card.destroy();
-  }
+      type: 'doughnut',
+      data: {
+          labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              hoverBackgroundColor: [
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56",
+                  "#FF6384",
+                  "#36A2EB",
+                  "#FFCE56"
+              ]
+          }]
+      }
 
-  onChange({ error }) {
-    if (error) {
-      this.error = error.message;
-    } else {
-      this.error = null;
+  });
+
+  this.lineChart = new Chart(this.lineCanvas.nativeElement, {
+
+      type: 'line',
+      data: {
+          labels: ["January", "February", "March", "April", "May", "June", "July"],
+          datasets: [
+              {
+                  label: "My First dataset",
+                  fill: false,
+                  lineTension: 0.1,
+                  backgroundColor: "rgba(75,192,192,0.4)",
+                  borderColor: "rgba(75,192,192,1)",
+                  borderCapStyle: 'butt',
+                  borderDash: [],
+                  borderDashOffset: 0.0,
+                  borderJoinStyle: 'miter',
+                  pointBorderColor: "rgba(75,192,192,1)",
+                  pointBackgroundColor: "#fff",
+                  pointBorderWidth: 1,
+                  pointHoverRadius: 5,
+                  pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                  pointHoverBorderColor: "rgba(220,220,220,1)",
+                  pointHoverBorderWidth: 2,
+                  pointRadius: 1,
+                  pointHitRadius: 10,
+                  data: [65, 59, 80, 81, 56, 55, 40],
+                  spanGaps: false,
+              }
+          ]
+      }
+
+  });
+
+}
+
+ngAfterViewInit() {
+  const style = {
+    base: {
+      lineHeight: '24px',
+      fontFamily: 'monospace',
+      fontSmoothing: 'antialiased',
+      fontSize: '19px',
+      '::placeholder': {
+        color: 'purple'
+      }
     }
-    this.cd.detectChanges();
+  };
+
+  this.card = elements.create('card', { style });
+  this.card.mount(this.cardInfo.nativeElement);
+
+  this.card.addEventListener('change', this.cardHandler);
+}
+
+ngOnDestroy() {
+  this.card.removeEventListener('change', this.cardHandler);
+  this.card.destroy();
+}
+
+onChange({ error }) {
+  if (error) {
+    this.error = error.message;
+  } else {
+    this.error = null;
   }
+  this.cd.detectChanges();
+}
 
-  async onSubmit(form: NgForm) {
-    const { token, error } = await stripe.createToken(this.card);
+async onSubmit(form: NgForm) {
+  const { token, error } = await stripe.createToken(this.card);
 
-    if (error) {
-      console.log('Something is wrong:', error);
-    } else {
-      console.log('Success!', token);
-      this.charge_id = token.id;
-      
-      this.payment();
-      // ...send the token to the your backend to process the charge
-    } 
+  if (error) {
+    console.log('Something is wrong:', error);
+  } else {
+    console.log('Success!', token);
+    this.charge_id = token.id;
+
+    this.payment();
+    // ...send the token to the your backend to process the charge
   }
+}
 
-  payment() {
-    this.http
+payment() {
+  this.http
     .post("http://localhost:3000/payment?jwt=" + localStorage.getItem("TOKEN"), {
       //firstname: this.firstname,
       //lastname: this.lastname,
@@ -98,21 +209,23 @@ export class PaymentPage implements AfterViewInit, OnDestroy {
       //receipt_email: this.receipt_email,
     })
     .subscribe(
-      result=> {
+      result => {
         console.log(result);
 
 
 
-        this.navCtrl.push("");
+        this.navCtrl.push("MapPage");
       },
 
       err => {
         console.log(err);
       }
     );
-  }
+}
 
 ionViewLoad() {
   console.log('ionViewDidLoad MapPage');
-  }
 }
+}
+
+
